40f00c1829c02577026ada04e388ec2e06345807
Kingsley B. Boafo <109430532+kayc0des@users.noreply.github.com>
2023-09-07 20:24:04 +0200

    dart logic

--- Lessonx3a_dart/05_loops.dart
+++ Lessonx3a_dart/05_loops.dart
@@ -1,7 +1,19 @@
 void main() {
   // For Loop
+  for (int i = 5; i >= 1; i--) {
+    print(i);
+  }
 
   // For In Loop
+  var names = ["John", "Tina", "Steve"];
+  for (var name in names) {
+    print(name);
+  }
 
   // While Loop
+  var num = 5;
+  while (num >= 1) {
+    print(num);
+    num--;
+  }
 }
--- Lessonx3a_dart/06_logic.dart
+++ Lessonx3a_dart/06_logic.dart
@@ -1,3 +1,24 @@
 void main() {
-  /// code
+  // Logic: If, If/Else, If Else If
+
+  String firstName = "Alex";
+  var subject = [
+    "Computer Science",
+    "Marketing",
+    "Accounting",
+    "Web Enterprise"
+  ];
+
+  // If Statement
+  if (firstName == "Alex") {
+    print("Hello Alex, this is an introduction to dart");
+  }
+
+  // if Else
+  if (subject[0] == "Accounting") {
+    print("The first item in the subject list is accounting");
+  } else {
+    var firstItem = subject[0];
+    print("The first item in the subject list is $firstItem");
+  }
 }
